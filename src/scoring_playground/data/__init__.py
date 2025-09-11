d589710e1ddbd421c5eee721ad900dac1fc80d3c
Louis Burda <contact@sinitax.com>
2025-09-11 15:48:50 +0200

    Add faust2024 dataset

--- src/scoring_playground/data/__init__.py
+++ src/scoring_playground/data/__init__.py
@@ -6,12 +6,15 @@ class SaarCTF2024(JSONDataSource, file_name='saarctf2024.json'):
     '''CTF data from saarCTF 2024'''
 
 class ENOWARS2024(JSONDataSource, file_name='enowars2024.json'):
-    '''CTF data from saarCTF 2024'''
+    '''CTF data from ENOWARS (8) 2024'''
 
+class FaustCTF2024(JSONDataSource, file_name='faustctf2024.json'):
+    '''CTF data from FaustCTF 2024'''
 
 sources: list[type[DataSource]] = [
     ENOWARS2024,
     SaarCTF2024,
+    FaustCTF2024
 ]
-__all__ = ['sources', 'ENOWARS2024', 'SaarCTF2024']
+__all__ = ['sources', 'ENOWARS2024', 'SaarCTF2024', 'FaustCTF2024']
 
